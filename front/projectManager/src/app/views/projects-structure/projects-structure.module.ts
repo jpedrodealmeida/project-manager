ef0bdf8e28fee79564488f963161550ab912ae33
Joao Pedro <joao.sjc12@hotmail.com>
2020-06-01 17:55:13 -0300

    Creating main menu

--- front/projectManager/src/app/views/projects-structure/projects-structure.module.ts
+++ front/projectManager/src/app/views/projects-structure/projects-structure.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProjectsStructureComponent } from './projects-structure.component';
 import {MatCardModule} from '@angular/material/card';
-
+import {MatTabsModule} from '@angular/material/tabs';
 
 @NgModule({
   declarations: [
@@ -10,7 +10,8 @@ import {MatCardModule} from '@angular/material/card';
   ],
   imports: [
     CommonModule,
-    MatCardModule
+    MatCardModule,
+    MatTabsModule
   ],
   exports: [
     ProjectsStructureComponent
