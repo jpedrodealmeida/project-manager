92419673bc92aab780954694a9be4633c6fb870e
Joao Pedro <joao.sjc12@hotmail.com>
2020-06-01 16:28:18 -0300

    GET all projects and POST new project

--- back/index.js
+++ back/index.js
@@ -4,4 +4,21 @@ const server = express();
 
 server.use(express.json());
 
+const project = []
+
+server.get('/projects', (req, res) =>{
+    return res.json(project)
+})
+
+server.post('/projects', (req, res) => {
+    const { id } = req.body
+    const { title } = req.body
+
+    project.push({id: id, title: title, tasks: []})
+
+    return res.json({Message: "Success created", project})
+
+})
+
+
 server.listen(3000)
