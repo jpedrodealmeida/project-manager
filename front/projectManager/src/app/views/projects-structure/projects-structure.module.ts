fd5d641714c4df28b9f64286addca2c45f34dcaf
Joao Pedro <joao.sjc12@hotmail.com>
2020-06-01 18:03:12 -0300

    creating project service and project list comp

--- front/projectManager/src/app/views/projects-structure/projects-structure.module.ts
+++ front/projectManager/src/app/views/projects-structure/projects-structure.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { ProjectsStructureComponent } from './projects-structure.component';
 import {MatCardModule} from '@angular/material/card';
 import {MatTabsModule} from '@angular/material/tabs';
+import { ProjectListComponent } from './project-list/project-list.component';
 
 @NgModule({
   declarations: [
-    ProjectsStructureComponent
+    ProjectsStructureComponent,
+    ProjectListComponent
   ],
   imports: [
     CommonModule,
