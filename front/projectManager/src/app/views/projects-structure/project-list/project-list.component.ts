f396a290be7d7db969bc7e7398da6eb6df90104c
Joao Pedro <joao.sjc12@hotmail.com>
2020-06-02 10:48:43 -0300

    component structure

--- front/projectManager/src/app/views/projects-structure/project-list/project-list.component.ts
+++ front/projectManager/src/app/views/projects-structure/project-list/project-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-project-list',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./project-list.component.scss']
 })
 export class ProjectListComponent implements OnInit {
+
+  @Input('project') project
 
   constructor() { }
 
--- front/projectManager/src/app/views/projects-structure/projects-structure.component.ts
+++ front/projectManager/src/app/views/projects-structure/projects-structure.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Project {
+  title: string,
+  id: number,
+  tasks: []
+}
 @Component({
   selector: 'app-projects-structure',
   templateUrl: './projects-structure.component.html',
@@ -7,9 +12,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectsStructureComponent implements OnInit {
 
+  public projects
+
   constructor() { }
 
   ngOnInit() {
+    this.projects = 
+    [
+      {title: "Angular", id: 0, tasks: ['Create', 'Read', 'Update', 'Delete']},
+      {title: "React", id: 1, tasks: ['Create', 'Read', 'Update', 'Delete']}
+    ]
   }
 
 }
