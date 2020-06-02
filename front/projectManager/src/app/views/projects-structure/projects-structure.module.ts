2c7946d4e2db85fd16636f686de9da082f58cf76
Joao Pedro <joao.sjc12@hotmail.com>
2020-06-02 12:20:44 -0300

    form structure about new project

--- front/projectManager/src/app/views/projects-structure/projects-structure.module.ts
+++ front/projectManager/src/app/views/projects-structure/projects-structure.module.ts
@@ -4,16 +4,27 @@ import { ProjectsStructureComponent } from './projects-structure.component';
 import {MatCardModule} from '@angular/material/card';
 import {MatTabsModule} from '@angular/material/tabs';
 import { ProjectListComponent } from './project-list/project-list.component';
+import { ProjectFormComponent } from './project-form/project-form.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
     ProjectsStructureComponent,
-    ProjectListComponent
+    ProjectListComponent,
+    ProjectFormComponent
   ],
   imports: [
     CommonModule,
     MatCardModule,
-    MatTabsModule
+    MatTabsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDividerModule,
+    MatButtonModule
+    
   ],
   exports: [
     ProjectsStructureComponent
